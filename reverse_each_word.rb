ab77c79971f9702d5105e9b9800a7d8c5ec38d91
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 22:06:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word(str)
   new =[]
   str_to_array = str.split("")
-  p str_to_array
-  str_to_array.length.times do|index|
-  new[index] = str_to_array[index].reverse
- # p "#{new[index]}"
-  end
-    #str_to_array.reverse
-    new.join
+
 end
