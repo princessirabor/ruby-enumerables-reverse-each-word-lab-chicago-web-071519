8b6a0cb0202d9b408e41f4f92f47c755acbe029e
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 22:37:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,10 @@ def reverse_each_word(str)
   a = str.split(" ")
   p a[0].reverse
   store = []
+  i = 0
   a.each do |this_a|
-     store = this_a.reverse
+     store[i] = this_a.reverse
+     i= i+1
   end
 store
 end
